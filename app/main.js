483d2368c565186d5caa822ce5a2fd700a8ab0fc
thukuwanjiku <mikedegr809@gmail.com>
2018-11-16 19:00:05 +0300

    Boilerplate for side drawer

---
+++ app/main.js
@@ -0,0 +1,26 @@
+import Vue from 'nativescript-vue'
+import routes from './router'
+import store from './store'
+import sideDrawer from './components/SideDrawer'
+import drawerContent from './components/DrawerContent'
+
+// Prints Vue logs when --env.production is *NOT* set while building
+Vue.config.silent = (TNS_ENV === 'production')
+
+Vue.registerElement('RadSideDrawer', () => require('nativescript-ui-sidedrawer').RadSideDrawer)
+
+// Set up routes as a prototype to use throuhout the app.
+Vue.prototype.$routes = routes
+
+new Vue({
+    store,
+    render (h) {
+        return h(
+            sideDrawer,
+            [
+                h(drawerContent, { slot: 'drawerContent' }),
+                h(routes.Home, { slot: 'mainContent' })
+            ]
+        )
+    }
+}).$start()
---
+++ app/router/index.js
@@ -0,0 +1,13 @@
+import Home from './../components/Home'
+import PageOne from './../components/PageOne'
+import PageTwo from './../components/PageTwo'
+import PageThree from './../components/PageThree'
+
+const routes = {
+    Home,
+    PageOne,
+    PageTwo,
+    PageThree,
+}
+
+export default routes
